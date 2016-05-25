157da41af2f84d2f4baf0f58db5a3ef65e0555c5
TASHIRO Katsuya <tashiro@syslabo.co.jp>
2016-05-25 22:31:33 +0900

    Change version to revision

--- open-usp-tukubai.rb
+++ open-usp-tukubai.rb
@@ -1,7 +1,7 @@
 class OpenUspTukubai < Formula
   desc "Open usp Tukubai"
   homepage "https://uec.usp-lab.com/TUKUBAI/CGI/TUKUBAI.CGI?POMPA=DOWNLOAD"
-  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :version => "0da8b6cfe04df6e7d8cb8b4fe62170ee2fc4ab47"
+  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :revision => "0da8b6cfe04df6e7d8cb8b4fe62170ee2fc4ab47"
   version "20151015"
   head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :branch => "master"
 
