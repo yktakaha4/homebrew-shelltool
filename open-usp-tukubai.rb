a90e58f0b24dbf8aaeae523c86f0fd831645fdd8
TASHIRO Katsuya <tashiro@syslabo.co.jp>
2016-05-25 20:34:02 +0900

    Specify version

--- open-usp-tukubai.rb
+++ open-usp-tukubai.rb
@@ -1,9 +1,9 @@
 class OpenUspTukubai < Formula
   desc "Open usp Tukubai"
   homepage "https://uec.usp-lab.com/TUKUBAI/CGI/TUKUBAI.CGI?POMPA=DOWNLOAD"
-  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git"
+  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :version => "0da8b6cfe04df6e7d8cb8b4fe62170ee2fc4ab47"
   version "20151015"
-  head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git"
+  head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :branch => "master"
 
   def install
     ENV.append "LOCALBASE", "/usr/local/Cellar/open-usp-tukubai/#{version}"
