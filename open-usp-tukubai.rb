b9ed376b5436d540097d3cf78fde6d525357b991
papiron <Katsuya.TASHIRO@gmail.com>
2017-11-26 16:51:09 +0900

    Update to 2017-07-06

--- open-usp-tukubai.rb
+++ open-usp-tukubai.rb
@@ -1,8 +1,8 @@
 class OpenUspTukubai < Formula
   desc "Open usp Tukubai"
   homepage "https://uec.usp-lab.com/TUKUBAI/CGI/TUKUBAI.CGI?POMPA=DOWNLOAD"
-  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :revision => "0da8b6cfe04df6e7d8cb8b4fe62170ee2fc4ab47"
-  version "20151015"
+  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :revision => "7a91a42a09f499bff947ce021454713c46b45775"
+  version "20170706"
   head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :branch => "master"
 
   def install
