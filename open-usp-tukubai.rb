ee7b1c11df8f935d57b403f550c2ee3222623796
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-09 03:31:24 +0900

    update makefile

--- open-usp-tukubai.rb
+++ open-usp-tukubai.rb
@@ -1,9 +1,9 @@
 class OpenUspTukubai < Formula
   desc "Open usp Tukubai"
   homepage "https://uec.usp-lab.com/TUKUBAI/CGI/TUKUBAI.CGI?POMPA=DOWNLOAD"
-  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :revision => "7a91a42a09f499bff947ce021454713c46b45775"
+  url "https://github.com/yktakaha4/Open-usp-Tukubai.git", :revision => "79e64aaa92b72b2be82a075cfd96d8d15ae806d2"
   version "20170706"
-  head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", :branch => "master"
+  head "https://github.com/yktakaha4/Open-usp-Tukubai.git", :branch => "master"
 
   def install
     ENV.append "LOCALBASE", prefix
